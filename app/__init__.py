b9618157312e9c96b8861a2c909e80b15ad4cd24
dorianinc <dorianrdba@gmail.com>
2024-02-02 03:19:28 -0800

    removed unecessary imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,13 @@
 import os
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect, generate_csrf
-from flask_login import LoginManager
+from flask_wtf.csrf import generate_csrf
 from .models import db
 from .seeds import seed_commands
 from .config import Config
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
-
-# Setup login manager
-login = LoginManager(app)
-login.login_view = 'auth.unauthorized'
-
-
-# @login.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
 
 
 # Tell flask about our seed commands
