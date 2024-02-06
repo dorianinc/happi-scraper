247d95b5d50747742b78fa56366e3d917f1cd74a
dorianinc <dorianrdba@gmail.com>
2024-02-05 18:57:18 -0800

    got matching function up and running

--- app/helpers/helpers.py
+++ app/helpers/helpers.py
@@ -4,7 +4,11 @@ from fuzzywuzzy import fuzz
 def match_products(name1, name2):
     normalized_name1 = name1.lower().strip()
     normalized_name2 = name2.lower().strip()
+    print(f"==>> normalized_name1: {normalized_name1}")
+    print(f"==>> normalized_name2: {normalized_name2}")
     similarity = fuzz.ratio(normalized_name1, normalized_name2)
-    threshold = 0.7  # Adjust as needed based on your data
+    print(f"==>> similarity: {similarity}")
+    threshold = 80  # Adjust as needed based on your data
     return similarity >= threshold
 
+match_products("Dragon Ball Z Solid Edge Works vol.5 (A: Super Saiyan 2 Son Gohan )", "Banpresto Dragon Ball Z Solid Edge Works vol.5(A:Super Saiyan 2 Son Gohan)")
