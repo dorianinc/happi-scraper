1af35e416e3d8e06b57ea3caee404b5a6f07350a
dorianinc <dorianrdba@gmail.com>
2024-02-14 14:18:09 -0800

    updated db

--- app/seeds/websites.py
+++ app/seeds/websites.py
@@ -3,19 +3,19 @@ from sqlalchemy.sql import text
 
 def seed_websites():
     websites_data = [
-        {'name': 'Super Anime Store', 'url': 'https://Superanimestore.com'},
-        {'name': 'eBay', 'url': 'https://eBay.com'},
-        {'name': 'Amazon', 'url': 'https://amazon.com'},
         {'name': 'AAA Anime', 'url': 'https://AAAanime.com'},
-        {'name': 'Entertainment Earth', 'url': 'https://entertainmentearth.com'},
+        {'name': 'Amazon', 'url': 'https://amazon.com'},
+        {'name': 'Big Bad Toy Store', 'url': 'https://bigbadtoystore.com'},
         {'name': 'Crunchyroll Store', 'url': 'https://store.crunchyroll.com'},
+        {'name': 'eBay', 'url': 'https://eBay.com'},
+        {'name': 'Entertainment Earth', 'url': 'https://entertainmentearth.com'},
         {'name': 'GK Figure Worldwide', 'url': 'https://gkfigureworldwide.com'},
         {'name': 'HLJ', 'url': 'https://hlj.com'},
+        {'name': 'Japan Figure', 'url': 'https://japan-figure.com'},
+        {'name': 'Kotous', 'url': 'https://kotous.com'},
         {'name': 'Otaku Mode', 'url': 'https://otakumode.com'},
         {'name': 'Solaris Japan', 'url': 'https://solarisjapan.com'},
-        {'name': 'Japan Figure', 'url': 'https://japan-figure.com'},
-        {'name': 'Big Bad Toy Store', 'url': 'https://bigbadtoystore.com'},
-        {'name': 'Kotous', 'url': 'https://kotous.com'}
+        {'name': 'Super Anime Store', 'url': 'https://Superanimestore.com'}
     ]
 
     all_websites = [Website(name=website['name'], url=website['url'])
@@ -30,3 +30,4 @@ def undo_websites():
     else:
         db.session.execute(text("DELETE FROM websites"))
     db.session.commit()
+
