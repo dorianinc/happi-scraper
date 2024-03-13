2cdc9ef273da6deb7ff369f7bf7f258a9dd5d2e1
Dorian Macias <dorianrdba@gmail.com>
2024-03-12 20:42:16 -0700

    restructured the websites table, seeder and config file

--- app/models/website.py
+++ app/models/website.py
@@ -1,5 +1,3 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-
 class Website(db.Model):
     __tablename__ = "websites"
 
@@ -7,15 +5,38 @@ class Website(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    url = db.Column(db.Text, nullable=False)
-    matches = db.relationship("Match", back_populates="website")
-    
+    name = db.Column(db.Text, nullable=False)  # Name of the website
+    url = db.Column(db.Text, nullable=False)  # URL of the website
+    search_bar_locator = db.Column(db.Text, nullable=True)  # Locator for search bar
+    header_locator = db.Column(db.Text, nullable=True)  # Locator for header
+    price_locator = db.Column(db.Text, nullable=True)  # Locator for price
+    pop_up_locator = db.Column(db.Text, nullable=True)  # Locator for pop-up if any
+    pop_up_check = db.Column(db.Boolean, nullable=False, default=False)  # Flag indicating whether pop-up is checked
+    search_button_locator = db.Column(db.Text, nullable=True)  # Locator for search button
+    search_button_check = db.Column(db.Boolean, nullable=False, default=False)
+    image_locator = db.Column(db.Text, nullable=True)  # Locator for product images
+    url_locator = db.Column(db.Text, nullable=True)  # Locator for product URLs
+    filter_check = db.Column(db.Boolean, nullable=False, default=False)  # Flag indicating whether result filtering is checked
+    excluded = db.Column(db.Boolean, nullable=False, default=False)  # Whether the website is excluded from scraping
+    matches = db.relationship("Match", back_populates="website")  # Whether the website supports result filtering
 
     def to_dict(self, include_matches=False):
         website_dict = {
             "id": self.id,
-            "name": self.name,
-            "url": self.url
+            "name": self.name, 
+            "url": self.url,
+            "search_bar_locator": self.search_bar_locator,
+            "header_locator": self.header_locator,
+            "price_locator": self.price_locator,
+            "pop_up_locator": self.pop_up_locator,
+            "pop_up_check": self.pop_up_check, 
+            "search_button_locator": self.search_button_locator,
+            "search_button_check": self.search_button_check,
+            "image_locator": self.image_locator,
+            "url_locator": self.url_locator,
+            "filter_check": self.filter_check,
+            "excluded": self.excluded
         }
+        if include_matches:
+            website_dict["matches"] = [match.to_dict() for match in self.matches]
         return website_dict
--- app/seeds/websites.py
+++ app/seeds/websites.py
@@ -1,27 +1,225 @@
 from app.models import db, Website, environment, SCHEMA
 from sqlalchemy.sql import text
 
+
 def seed_websites():
     websites_data = [
-        {'name': 'AAA Anime', 'url': 'https://AAAanime.com'},
-        {'name': 'Amazon', 'url': 'https://amazon.com'},
-        {'name': 'Big Bad Toy Store', 'url': 'https://bigbadtoystore.com'},
-        {'name': 'Crunchyroll Store', 'url': 'https://store.crunchyroll.com'},
-        {'name': 'eBay', 'url': 'https://eBay.com'},
-        {'name': 'Entertainment Earth', 'url': 'https://entertainmentearth.com'},
-        {'name': 'GK Figure Worldwide', 'url': 'https://gkfigureworldwide.com'},
-        {'name': 'HLJ', 'url': 'https://hlj.com'},
-        {'name': 'Japan Figure', 'url': 'https://japan-figure.com'},
-        {'name': 'Kotous', 'url': 'https://kotous.com'},
-        {'name': 'Otaku Mode', 'url': 'https://otakumode.com'},
-        {'name': 'Solaris Japan', 'url': 'https://solarisjapan.com'},
-        {'name': 'Super Anime Store', 'url': 'https://superanimestore.com'}
+        {
+            "name": "AAA Anime",
+            "url": "https://AAAanime.com",
+            "search_bar_locator": None,
+            "header_locator": None,
+            "price_locator": None,
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": None,
+            "url_locator": None,
+            "filter_results": None,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": True
+        },
+        {
+            "name": "Amazon",
+            "url": "https://www.amazon.com",
+            "search_bar_locator": "input[name='field-keywords']",
+            "header_locator": ".s-title-instructions-style .a-color-base.a-text-normal",
+            "price_locator": ".srp-results .s-item__price",
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": ".s-product-image-container .s-image",
+            "url_locator": ".a-link-normal.s-no-outline",
+            "filter_results": False,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": False
+        },
+        {
+            "name": "Big Bad Toy Store",
+            "url": "https://bigbadtoystore.com",
+            "search_bar_locator": None,
+            "header_locator": None,
+            "price_locator": None,
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": None,
+            "url_locator": None,
+            "filter_results": None,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": True
+        },
+        {
+            "name": "Crunchyroll",
+            "url": "https://store.crunchyroll.com",
+            "search_bar_locator": "input[placeholder='Search apparel, figures, and more']",
+            "header_locator": ".pdp-link",
+            "price_locator": ".sales .value",
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": ".tile-image",
+            "url_locator": ".image-tile-container",
+            "filter_results": False,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": False
+        },
+        {
+            "name": "eBay",
+            "url": "https://www.ebay.com",
+            "search_bar_locator": "input[placeholder='Search for anything']",
+            "header_locator": ".srp-results .s-item__title",
+            "price_locator": ".srp-results .s-item__price",
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": ".srp-results .s-item__image img",
+            "url_locator": ".srp-results .s-item__image a",
+            "filter_results": True,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": False
+        },
+        {
+            "name": "Entertainment Earth",
+            "url": "https://entertainmentearth.com",
+            "search_bar_locator": None,
+            "header_locator": None,
+            "price_locator": None,
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": None,
+            "url_locator": None,
+            "filter_results": None,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": True
+        },
+        {
+            "name": "GK Figure Worldwide",
+            "url": "https://gkfigureworldwide.com",
+            "search_bar_locator": None,
+            "header_locator": None,
+            "price_locator": None,
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": None,
+            "url_locator": None,
+            "filter_results": None,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": True
+        },
+        {
+            "name": "HLJ",
+            "url": "https://hlj.com",
+            "search_bar_locator": None,
+            "header_locator": None,
+            "price_locator": None,
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": None,
+            "url_locator": None,
+            "filter_results": None,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": True
+        },
+        {
+            "name": "Japan Figure",
+            "url": "https://japan-figure.com",
+            "search_bar_locator": "input[placeholder='What are you looking for?']",
+            "header_locator": ".productitem--title",
+            "price_locator": ".price__current .money",
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": ".productitem--image-primary",
+            "url_locator": ".productitem--image-link",
+            "filter_results": False,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": False
+        },
+        {
+            "name": "Kotous",
+            "url": "https://kotous.com",
+            "search_bar_locator": "input[placeholder='Enter keywords to search...']",
+            "header_locator": ".product-item-link",
+            "price_locator": ".price-final_price .price",
+            "pop_up_locator": ".fancybox-close",
+            "search_button_locator": None,
+            "image_locator": ".product-image-photo",
+            "url_locator": ".product-item-link",
+            "filter_results": False,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": False
+        },
+        {
+            "name": "Otaku Mode",
+            "url": "https://otakumode.com",
+            "search_bar_locator": "input[placeholder='Search Products...']",
+            "header_locator": ".p-product-list__title",
+            "price_locator": ".p-price__regular",
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": ".p-product-list__item img",
+            "url_locator": ".p-product-list__title",
+            "filter_results": False,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": False
+        },
+        {
+            "name": "Solaris Japan",
+            "url": "https://solarisjapan.com",
+            "search_bar_locator": None,
+            "header_locator": ".title",
+            "price_locator": ".product-submit__btn--red .money",
+            "pop_up_locator": None,
+            "search_button_locator": None,
+            "image_locator": None,
+            "url_locator": None,
+            "filter_results": None,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": False,
+            "excluded": True
+        },
+        {
+            "name": "Super Anime Store",
+            "url": "https://superanimestore.com",
+            "search_bar_locator": "#Search-In-Modal-1",
+            "header_locator": ".h5 .full-unstyled-link",
+            "price_locator": ".price-item--regular",
+            "pop_up_locator": ".privy-x",
+            "search_button_locator": ".icon.icon-search",
+            "image_locator": ".card__media .motion-reduce",
+            "url_locator": ".card__information a",
+            "filter_results": False,
+            "pop_up_check": False,
+            "filter_check": False,
+            "search_button_check": True,
+            "excluded": False
+        }
+
     ]
 
     all_websites = [Website(name=website['name'], url=website['url'])
                     for website in websites_data]
     add_websites = [db.session.add(website) for website in all_websites]
     db.session.commit()
+
 
 def undo_websites():
     if environment == "production":
@@ -31,3 +229,18 @@ def undo_websites():
         db.session.execute(text("DELETE FROM websites"))
     db.session.commit()
 
+    # websites_data = [
+    #     {'name': 'AAA Anime', 'url': 'https://AAAanime.com'},  # 1
+    #     {'name': 'Amazon', 'url': 'https://amazon.com'}, # 2
+    #     {'name': 'Big Bad Toy Store', 'url': 'https://bigbadtoystore.com'}, # 3
+    #     {'name': 'Crunchyroll Store', 'url': 'https://store.crunchyroll.com'}, # 4
+    #     {'name': 'eBay', 'url': 'https://eBay.com'},  # 5
+    #     {'name': 'Entertainment Earth', 'url': 'https://entertainmentearth.com'}, # 6
+    #     {'name': 'GK Figure Worldwide', 'url': 'https://gkfigureworldwide.com'}, # 7
+    #     {'name': 'HLJ', 'url': 'https://hlj.com'}, # 8
+    #     {'name': 'Japan Figure', 'url': 'https://japan-figure.com'}, # 9
+    #     {'name': 'Kotous', 'url': 'https://kotous.com'}, # 10
+    #     {'name': 'Otaku Mode', 'url': 'https://otakumode.com'}, # 11
+    #     {'name': 'Solaris Japan', 'url': 'https://solarisjapan.com'}, # 12
+    #     {'name': 'Super Anime Store', 'url': 'https://superanimestore.com'} # 13
+    # ]
