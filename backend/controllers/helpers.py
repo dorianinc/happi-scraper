979eeff89663657485ad11cbdeb5a54cc1fd7594
Dorian Macias <dorianrdba@gmail.com>
2024-03-21 14:58:47 -0700

    changed sorting algo in match_prodcuts

--- backend/controllers/helpers.py
+++ backend/controllers/helpers.py
@@ -5,7 +5,7 @@ from thefuzz import process
 def match_products(name1, name2):
     normalized_name1 = name1.lower().strip()
     normalized_name2 = name2.lower().strip()
-    similarity = fuzz.partial_ratio(normalized_name1, normalized_name2)
+    similarity = fuzz.token_set_ratio(normalized_name1, normalized_name2)
     # print(f"==>> Name of product I'm looking up: {normalized_name1}")
     # print(f"==>> Name of product on website: {normalized_name2}")
     return similarity
