4710119b87ca35a6fc350c5fc24cc7d384caa8a4
Dorian Macias <dorianrdba@gmail.com>
2024-05-07 20:21:00 -0700

    got project back to base

--- main.js
+++ main.js
@@ -1,56 +1,40 @@
 const { app, BrowserWindow } = require('electron');
-const { exec } = require('child_process');
 
-function startBackend() {
-    // Start the backend on port 8000
-    const backendProcess = exec('cd backend && npm run start');
+let mainWindow;
 
-    // backendProcess.stdout.on('data', (data) => {
-    //     console.log(`Backend stdout: ${data}`);
-    // });
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    maxWidth: 1315,
+    maxHeight: 775,
+    minWidth: 1315,
+    minHeight: 775,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
 
-    // backendProcess.stderr.on('data', (data) => {
-    //     console.error(`Backend stderr: ${data}`);
-    // });
+  mainWindow.loadURL("http://localhost:3000");
 
-    // backendProcess.on('close', (code) => {
-    //     console.log(`Backend process exited with code ${code}`);
-    // });
+  // Open DevTools - Remove this line for production
+//   mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
-const createWindow = () => {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        maxWidth: 1315,
-        maxHeight: 775,
-        minWidth: 1315,
-        minHeight: 775,
-        webPreferences: {
-            enableRemoteModule: true
-        }
-    });
+app.on('ready', createWindow);
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-}
-
-app.whenReady().then(() => {
-    createWindow();
-    startBackend();
-
-    app.on('activate', () => {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
