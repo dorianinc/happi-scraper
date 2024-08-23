bfd5b106598935b46eddec5c514aed5ae06b5dbf
Dorian Macias <33275910+dorianinc@users.noreply.github.com>
2024-08-23 00:59:56 -0700

    Update match.js

--- ipc/match.js
+++ ipc/match.js
@@ -2,12 +2,22 @@ const { ipcMain } = require("electron");
 const { match } = require("../controller");
 
 const matchIPC = () => {
-  // Get matches for a product
-  ipcMain.handle("get-matches", async (_e) => {
+  // Get match by id
+  ipcMain.handle("get-single-match", async (_e, id) => {
     try {
       return await match.getProductMatches();
     } catch (error) {
       console.error("Error in get-matches IPC handler:", error);
+      throw error;
+    }
+  });
+
+    // Create a new match
+  ipcMain.handle("create-match", async (_e, data) => {
+    try {
+      return await match.createMatch(data);
+    } catch (error) {
+      console.error("Error in create-match IPC handler:", error);
       throw error;
     }
   });
