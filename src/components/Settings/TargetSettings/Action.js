9bafeacafd4ab1c317916dd8e09a3cb1611fcd34
Dorian Macias <dorianrdba@gmail.com>
2024-10-30 01:59:49 -0700

    added css to scriptitems

--- src/components/Settings/TargetSettings/Action.js
+++ src/components/Settings/TargetSettings/Action.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
-import "./styles/Action.css"
+import "./styles/Action.css";
 
 function Action({ columnName, item, index }) {
-
   return (
     <Draggable draggableId={item.id} index={index}>
       {(provided, snapshot) => (
@@ -13,12 +12,19 @@ function Action({ columnName, item, index }) {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          {`${columnName === "Scripts" ? `${index + 1}.` : ""} ${
-            item.content
-          }`}
+          {`${columnName === "Scripts" ? `${index + 1}.` : ""} ${item.content}`}
+          
+          {columnName === "Scripts" && (
+            <div className="script-actions">
+              <input type="text" placeholder="Search..." className="find-input" />
+              <button className="find-btn">Find</button>
+              <button className="delete-btn">Delete</button>
+            </div>
+          )}
         </div>
       )}
     </Draggable>
   );
 }
+
 export default Action;
