aef99cb0c9e75669c647eb88d46af492a0207ed3
Dorian Macias <dorianrdba@gmail.com>
2024-11-07 01:24:58 -0800

    making progress on rebuilding columns logic

--- src/components/Settings/ScriptBuilder/data/initialData.js
+++ src/components/Settings/ScriptBuilder/data/initialData.js
@@ -1,30 +1,23 @@
-const initialData = {
-  columns: {
-    actionsColumn: {
-      id: "actionsColumn",
-      title: "Actions",
-      items: [
-        { id: "a1", type: "click" },
-        { id: "a2", type: "fill" },
-        {
-          id: "a3",
-          type: "waitForTimeout",
-        },
-        {
-          id: "a4",
-          type: "waitForElement",
-        },
-        // { id: "a5", content: "Go to URL", type: "goto" },
-      ],
+export const actionsColumnData = {
+  id: "actionsColumn",
+  title: "Actions",
+  items: [
+    { id: "a1", type: "click" },
+    { id: "a2", type: "fill" },
+    {
+      id: "a3",
+      type: "waitForTimeout",
     },
-    scriptsColumn: {
-      id: "scriptsColumn",
-      title: "Scripts",
-      items: [
-
-      ],
+    {
+      id: "a4",
+      type: "waitForElement",
     },
-  },
+    // { id: "a5", content: "Go to URL", type: "goto" },
+  ],
 };
 
-export default initialData;
+export const scriptColumnData = {
+  id: "scriptsColumn",
+  title: "Scripts",
+  items: [],
+};
--- src/components/Settings/ScriptBuilder/index.js
+++ src/components/Settings/ScriptBuilder/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-import initialData from "./data/initialData";
+import { actionsColumnData, scriptColumnData } from "./data/initialData";
 import { useDispatch, useSelector } from "react-redux";
 import Column from "./Column";
 import { DragDropContext } from "react-beautiful-dnd";
@@ -16,15 +16,13 @@ import { useDarkMode } from "../../../context/DarkModeContext";
 function ScriptBuilder() {
   const dispatch = useDispatch();
   const [placeholderProps, setPlaceholderProps] = useState({});
-  const [columns, setColumns] = useState(initialData.columns);
-  const [actionColumn, setActionColumn] = useState({});
-  const [scriptColumn, setScriptColumn] = useState({});
+  const [columns, setColumns] = useState();
+  const [actionColumn, setActionColumn] = useState(actionsColumnData);
+  const [scriptColumn, setScriptColumn] = useState(scriptColumnData);
   const [script, setScript] = useState({});
+  console.log("🖥️  a glimpse of usssss: ", scriptColumn.items);
 
-  const scripts = useSelector((state) => {
-    console.log("state ==> ", state.script);
-    return Object.values(state.script.scripts);
-  });
+  const scripts = useSelector((state) => Object.values(state.script.scripts));
 
   // Fetch search scripts when the component is mounted
   useEffect(() => {
@@ -32,9 +30,7 @@ function ScriptBuilder() {
   }, [dispatch]);
 
   const handleSelect = async (scriptId) => {
-    console.log("🖥️  scriptId: ", scriptId);
     const script = await dispatch(getSingleScriptThunk(scriptId));
-    console.log("🖥️  script: ", script);
     setScript(script);
 
     setColumns((prevColumns) => ({
@@ -46,134 +42,127 @@ function ScriptBuilder() {
     }));
   };
 
-  const createPlaceholder = (result, position) => {
-    const queryAttr = "data-rbd-drag-handle-draggable-id";
-    const draggableId = result.draggableId;
+  // const createPlaceholder = (result, position) => {
+  //   const queryAttr = "data-rbd-drag-handle-draggable-id";
+  //   const draggableId = result.draggableId;
 
-    const sourceColumnId = result.source.droppableId;
-    const sourceColumn = columns[sourceColumnId];
-    const itemIndex = result[position].index;
-    const draggedItem = sourceColumn.items.find(
-      (item) => item.id === draggableId
-    );
+  //   const sourceColumnId = result.source.droppableId;
+  //   const sourceColumn = columns[sourceColumnId];
+  //   const itemIndex = result[position].index;
+  //   const draggedItem = sourceColumn.items.find(
+  //     (item) => item.id === draggableId
+  //   );
 
-    const placeholderText =
-      sourceColumnId === "scriptsColumn"
-        ? `${itemIndex + 1}. ${draggedItem.content}`
-        : draggedItem.content;
+  //   const placeholderText =
+  //     sourceColumnId === "scriptsColumn"
+  //       ? `${itemIndex + 1}. ${draggedItem.content}`
+  //       : draggedItem.content;
 
-    const domQuery = `[${queryAttr}='${draggableId}']`;
-    const draggedDOM = document.querySelector(domQuery);
+  //   const domQuery = `[${queryAttr}='${draggableId}']`;
+  //   const draggedDOM = document.querySelector(domQuery);
 
-    if (!draggedDOM) return;
+  //   if (!draggedDOM) return;
 
-    const { clientHeight, clientWidth } = draggedDOM;
-    const clientY =
-      parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingTop) +
-      [...draggedDOM.parentNode.children]
-        .slice(0, itemIndex)
-        .reduce((total, curr) => {
-          const style = curr.currentStyle || window.getComputedStyle(curr);
-          const marginBottom = parseFloat(style.marginBottom);
-          return total + curr.clientHeight + marginBottom;
-        }, 0);
+  //   const { clientHeight, clientWidth } = draggedDOM;
+  //   const clientY =
+  //     parseFloat(window.getComputedStyle(draggedDOM.parentNode).paddingTop) +
+  //     [...draggedDOM.parentNode.children]
+  //       .slice(0, itemIndex)
+  //       .reduce((total, curr) => {
+  //         const style = curr.currentStyle || window.getComputedStyle(curr);
+  //         const marginBottom = parseFloat(style.marginBottom);
+  //         return total + curr.clientHeight + marginBottom;
+  //       }, 0);
 
-    setPlaceholderProps({
-      display: "flex",
-      sourceColumnId,
-      placeholderText,
-      clientHeight,
-      clientWidth,
-      clientY,
-      clientX: parseFloat(
-        window.getComputedStyle(draggedDOM.parentNode).paddingLeft
-      ),
-    });
-  };
+  //   setPlaceholderProps({
+  //     display: "flex",
+  //     sourceColumnId,
+  //     placeholderText,
+  //     clientHeight,
+  //     clientWidth,
+  //     clientY,
+  //     clientX: parseFloat(
+  //       window.getComputedStyle(draggedDOM.parentNode).paddingLeft
+  //     ),
+  //   });
+  // };
 
-  const handleDragStart = useCallback(
-    (result) => {
-      if (!result.source) return;
-      createPlaceholder(result, "source");
-    },
-    [columns]
-  );
+  // const handleDragStart = useCallback(
+  //   (result) => {
+  //     if (!result.source) return;
+  //     createPlaceholder(result, "source");
+  //   },
+  //   [columns]
+  // );
 
-  const handleDragUpdate = useCallback(
-    (result) => {
-      if (!result.destination) return;
-      createPlaceholder(result, "destination");
-    },
-    [columns]
-  );
+  // const handleDragUpdate = useCallback(
+  //   (result) => {
+  //     if (!result.destination) return;
+  //     createPlaceholder(result, "destination");
+  //   },
+  //   [columns]
+  // );
 
   const handleDragEnd = useCallback(
     (result) => {
       setPlaceholderProps({});
       const { source, destination } = result;
-
+  
       // Exit if no destination or if dropped in the same place
-      console.log("🖥️  source.droppableId: ", source.droppableId);
-      if (!destination) return;
-      if (destination.droppableId === "actionsColumn") return;
-      if (
-        source.droppableId === destination.droppableId &&
-        source.index === destination.index
-      )
-        return;
-
-      const sourceColumn = columns[source.droppableId];
-      const destinationColumn = columns[destination.droppableId];
-
+      if (invalidDrop(source, destination)) return;
+      console.log("scriptColumn ====> ---->", scriptColumn)
+  
       // Moving within the same column
-      if (sourceColumn.id === "scriptsColumn") {
-        const newItems = Array.from(sourceColumn.items);
+      if (source.droppableId === "scriptsColumn") {
+        const newItems = Array.from(scriptColumn.items);
         const [movedItem] = newItems.splice(source.index, 1);
         newItems.splice(destination.index, 0, movedItem);
-
-        setColumns((prevColumns) => ({
+  
+        setScriptColumn((prevColumns) => ({
           ...prevColumns,
-          [sourceColumn.id]: {
-            ...sourceColumn,
-            items: newItems,
-          },
+          items: newItems,
         }));
         return;
       }
-
+  
       // Moving from "Actions" to "Scripts" should add a new item in "Scripts" but not remove it from "Actions"
-      const draggedItem = sourceColumn.items[source.index];
+      const draggedItem = actionColumn.items[source.index];
       const newScriptItem = {
         ...draggedItem,
-        id: uuidv4(),
+        id: uuidv4(), // Ensure new unique ID for each new item
         step: destination.index,
         locator: null,
       };
-
+      console.log("🖥️  newScriptItem: ", newScriptItem);
+  
       // Create a new array for the "Scripts" column to add the dragged item
-      const newDestinationItems = Array.from(destinationColumn.items);
+      const newDestinationItems = Array.from(scriptColumn.items);
+      console.log("🖥️  destination.index: ", destination.index);
       newDestinationItems.splice(destination.index, 0, newScriptItem);
-
-      setColumns((prevColumns) => ({
+  
+      setScriptColumn((prevColumns) => ({
         ...prevColumns,
-        [sourceColumn.id]: {
-          ...sourceColumn,
-          items: sourceColumn.items, // Ensure the "Actions" column remains the same
-        },
-        [destinationColumn.id]: {
-          ...destinationColumn,
-          items: newDestinationItems,
-        },
+        items: newDestinationItems,
       }));
     },
-    [columns]
+    [actionColumn.items, scriptColumn.items] // Depend on the actual state values
   );
+
+  const invalidDrop = (source, destination) => {
+    if (!destination) return true;
+    if (destination.droppableId === "actionsColumn") return true;
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    )
+      return true;
+  };
 
   return (
     <DragDropContext
-      onDragEnd={handleDragEnd}
-      onDragUpdate={handleDragUpdate}
-      onDragStart={handleDragStart}
+      onDragEnd={(result) => handleDragEnd(result)}
+      // onDragUpdate={handleDragUpdate}
+      // onDragStart={handleDragStart}
     >
       <DropdownButton id="dropdown-item-button" title="Select Site">
         {scripts.map((script) => (
@@ -190,17 +179,24 @@ function ScriptBuilder() {
         style={{ display: "flex", justifyContent: "center" }}
         className="drag-drop-container"
       >
-        {Object.entries(columns).map(([columnId, column]) => (
-          <Column
-            key={columnId}
-            columnId={columnId}
-            column={column}
-            script={script}
-            items={column.items}
-            setColumns={setColumns}
-            placeholderProps={placeholderProps}
-          />
-        ))}
+        <Column
+          key={actionColumn.id}
+          columnId={actionColumn.id}
+          column={actionColumn}
+          script={script}
+          items={actionColumn.items}
+          setColumns={setActionColumn}
+          placeholderProps={placeholderProps}
+        />
+        <Column
+          key={scriptColumn.id}
+          columnId={scriptColumn.id}
+          column={scriptColumn}
+          script={script}
+          items={scriptColumn.items}
+          setColumns={setScriptColumn}
+          placeholderProps={placeholderProps}
+        />
       </div>
     </DragDropContext>
   );
